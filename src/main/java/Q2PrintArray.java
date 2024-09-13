36df9eeb83bf055e0b3b36dce1cc75e621edbcaf
ms943533 <157423961+ms943533@users.noreply.github.com>
2024-09-13 18:20:24 -0400

    Update Q2PrintArray.java

--- src/main/java/Q2PrintArray.java
+++ src/main/java/Q2PrintArray.java
@@ -2,16 +2,16 @@ public class Q2PrintArray {
     private static int[] array = {1, 2, 3, 4, 5, 6, 7};
 
     public static void main(String[] args) {
-        printRecursion(array.length);
+        printRecursion(0);
     }
 
     private static void printRecursion(int i) {
         // TODO: Implement the recursive method to print array elements
-        if(i <= 0){
-            return 0;
+        if(i >= array.length){
+            return;
         }
-        System.out.println(array[i-1]);
-        printRecursion(i-1);
+        System.out.println(array[i]);
+        printRecursion(i+1);
         // Hint: The base case is when i <= 0
         // Hint: Print the element before the recursive call
     }
