2da31f0591c213c71331185a1a5a4cad24bc2411
ms943533 <157423961+ms943533@users.noreply.github.com>
2024-09-13 20:47:01 -0400

    Update Q1WeirdFunction.java

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -13,19 +13,19 @@ public class Q1WeirdFunction {
         if (n < 3) {
             return n;
         }
-        
-        int f0 = 0;
-        int f1 = 1;
-        int f2 = 2;
-        int fn = 0;
+        int[] f = new int[n + 1];
+        f[0] = 0;
+        f[1] = 1;
+        f[2] = 2;
 
         for(int i = 3; i <= n; i++){
-            fn = f2 + 2 * f1 + 3 * f0;
-            
-            f0 = f1;
-            f1 = f2;
-            f2 = fn;
+            f[i] = f[i - 1] + 2 * f[i - 2] + 3 * f[i - 3];
         }
-        return fn;
+        return f[n];
+    }
+    public static void main(String[] args){
+        int n = 10;
+        System.out.println(n + " " + fRecursive(n));
+        System.out.println(n + " " + fIterative(n));
     }
 }
